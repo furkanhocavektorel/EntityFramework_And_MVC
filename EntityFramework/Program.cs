bbbdf64da5b460bd0257dfab2c431142536a18a5
OGRETMEN-PC <OGRETMEN-PC@LAB11-OGRETMEN>
2024-05-25 13:06:48 +0300

    Add project files.

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,61 @@
+﻿using EntityFramework.context;
+using EntityFramework.entity;
+
+namespace EntityFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            NorthwindContext furkan = new NorthwindContext();
+
+
+            //DbSet<Category> ct = furkan.Categories;
+            //List<Category> list = furkan.Categories.ToList();
+
+        
+
+            //List<Product> products = furkan.Products
+            //   .Where(axa=> axa.UnitsInStock>110).ToList();
+
+            //Product p=furkan.Products.FirstOrDefault(x => x.UnitsInStock > 100);
+
+            //try{
+            //    Product p2 = furkan.Products.SingleOrDefault(x => x.UnitsInStock > 100);
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e.Message);    
+            //}
+
+            //List<Product> list2= furkan.Products.OrderBy(x=>x.ProductName).ToList();
+            //List<Product> list3 = furkan.Products.OrderByDescending(x => x.ProductName).ToList();
+
+            //bool varMi = furkan.Products.Any(x => x.UnitsInStock > 100);
+
+            //bool varMi2= furkan.Products.All(x=> x.UnitsInStock > 100);
+            //int kayitSayisi= furkan.Products.Count(x=> x.UnitPrice>20);
+
+        //    List<Product> products= 
+        // furkan.Products.Include("CategoryId").ToList();
+
+        //    Product p = furkan.Products
+        //        .FirstOrDefault(x => x.UnitPrice > 50);
+        //    Console.WriteLine(p.ProductName);
+        //    Console.WriteLine(p.CategoryId.CategoryName);
+
+        //    Category c = furkan.Categories
+        //.SingleOrDefault(x => x.CategoryID == p.ProductID);
+        //    Console.WriteLine(c.CategoryName);
+
+
+            List<Product> products = 
+                furkan.Products.Where(x=>x.UnitPrice>15).ToList();
+
+            furkan.Products.ToList().Where(x => x.UnitPrice > 15);
+
+
+        }
+    }
+}
---
+++ EntityFramework/context/NorthwindContext.cs
@@ -0,0 +1,25 @@
+﻿using EntityFramework.entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.context
+{
+    public class NorthwindContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=LAB11-OGRETMEN\\SQLEXPRESS;" +
+                "database=csharp215;Trusted_connection=true;" +
+                "TrustServerCertificate=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
